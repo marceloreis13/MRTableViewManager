6b0c4085420828e11f08e86a9fa7100a6f046066
Marcelo Reis <marcello@marcelloreis.com>
2016-06-25 13:06:47 -0300

    Initial commit

---
+++ Example/MRTableViewManager/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MRTableViewManager
+//
+//  Created by Marcelo Reis on 06/25/2016.
+//  Copyright (c) 2016 Marcelo Reis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
